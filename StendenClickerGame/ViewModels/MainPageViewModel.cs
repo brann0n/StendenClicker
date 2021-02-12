25c4a1fe7286ab2c6aa6cf97c1b18b245f6be836
Jarno <jarnohilverts@gmail.com>
2021-02-12 15:21:25 +0100

    Added viewmodel and menu items

---
+++ StendenClickerGame/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StendenClickerGame.ViewModels
+{
+    public class MainPageViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// The important notifier method of changed properties. This function should be called whenever you want to inform other classes that some property has changed.
+		/// </summary>
+		/// <param name="propertyName">The name of the updated property. Leaving this blank will fill in the name of the calling property.</param>
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+	
+}
