58736a944a7e1baa22770d5c95e5556e40659aef
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2021-03-11 15:05:11 +0100

    Update ReusableCurrencyPool.cs
    created ObjectPool for the currency

--- StendenClicker.Library/CurrencyObjects/ReusableCurrencyPool.cs
+++ StendenClicker.Library/CurrencyObjects/ReusableCurrencyPool.cs
@@ -1,53 +1,114 @@
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace StendenClicker.Library.CurrencyObjects
 {
     public class ReusableCurrencyPool
     {
-        private static readonly Lazy<ReusableCurrencyPool> instance = new Lazy<ReusableCurrencyPool>(() => new ReusableCurrencyPool());
-        protected static ReusableCurrencyPool Instance { get { return instance.Value; } }
+        //private static readonly Lazy<ReusableCurrencyPool> instance = new Lazy<ReusableCurrencyPool>(() => new ReusableCurrencyPool());
+        //protected static ReusableCurrencyPool Instance { get { return instance.Value; } }
+        //
+        //private List<Currency> reusables;
+        //private int PoolSizeSC;
+        //private int PoolSizeEC;
+        //private static int PoolSizeSC_MAX = 5;
+        //private static int PoolSizeEC_MAX = 5;
+        //
+        //
+        //private ReusableCurrencyPool()
+        //{
+        //    PoolSizeSC = 0;
+        //    PoolSizeEC = 0;
+        //
+        //    reusables = new List<Currency>();
+        //}
+        //
+        //public static ReusableCurrencyPool getInstance()
+        //{
+        //    return instance.Value;
+        //}
+        //
+        //public Currency aquireReusable()
+        //{
+        //    return null;
+        //}
+        //
+        //public Currency releaseReusable(int index)
+        //{
+        //    return null;
+        //}
+        //
+        //public void setMaxPoolSize(int scSize, int ecSize)
+        //{
+        //
+        //}
+        //
+        //public void visualPoolSize(int size)
+        //{
+        //
+        //}
 
-        private List<Currency> reusables;
+        private ConcurrentBag<SparkCoin> _sparkCoinBag;
+        private ConcurrentBag<EuropeanCredit> _europeanCreditsBag;
         private int PoolSizeSC;
         private int PoolSizeEC;
+        private static int MaxSparkCoinCount = 10;
+        private static int MaxEuropeanCreditCount = 5;
 
-
-        private ReusableCurrencyPool()
+        public ReusableCurrencyPool()
         {
             PoolSizeSC = 0;
             PoolSizeEC = 0;
-
-            reusables = new List<Currency>();
+            _sparkCoinBag = new ConcurrentBag<SparkCoin>();
+            _europeanCreditsBag = new ConcurrentBag<EuropeanCredit>();
         }
 
-        public static ReusableCurrencyPool getInstance()
+        // returns a SparkCoin object so it can be dropped on the platform after defeating
+        // a monster
+        // TODO convert to Currency based function.
+        public SparkCoin GetSparkCoin()
         {
-            return instance.Value;
+            SparkCoin sparkcoin;
+            if(PoolSizeSC > 0)
+            {
+                if (_sparkCoinBag.TryTake(out sparkcoin))
+                {
+                    return sparkcoin;
+                }
+                else
+                {
+                    if (PoolSizeSC < MaxSparkCoinCount)
+                    {
+                        sparkcoin = createSparkCoin();
+                    } 
+                    else
+                    {
+                        //TODO Force Release a coin so it can be dropped with a new value
+                    }
+                }
+            }
+            else
+            {
+                sparkcoin = createSparkCoin();
+            }
+            return sparkcoin;
         }
 
-        public Currency aquireReusable()
+        // creates a SparkCoin Object so it can be added to the ObjectPool
+        private SparkCoin createSparkCoin()
         {
-            return null;
-        }
+            SparkCoin newSparkcoin = new SparkCoin();
+            PoolSizeSC++;
+            return newSparkcoin;
+        } 
 
-        public Currency releaseReusable(int index)
+        // adds the picked up spark coin back to the bag
+        public void ReleaseSparkCoin(SparkCoin sparkcoin)
         {
-            return null;
+            _sparkCoinBag.Add(sparkcoin);
         }
-
-        public void setMaxPoolSize(int scSize, int ecSize)
-        {
-
-        }
-
-        public void visualPoolSize(int size)
-        {
-
-        }
-
     }
-
 }
 
