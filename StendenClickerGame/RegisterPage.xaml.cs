71ef0f470532e52593b17f8b9d2b5fb3dba94b61
Jarno <jarnohilverts@gmail.com>
2021-03-24 15:37:21 +0100

    Changed design layout and added create account screen

--- StendenClickerGame/RegisterPage.xaml.cs
+++ StendenClickerGame/RegisterPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using StendenClickerGame.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,25 +13,25 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace StendenClickerGame
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class RegisterPage : Page
     {
-        public MainPage()
+        public RegisterPage()
         {
             this.InitializeComponent();
 
-            this.switchscreen.Click += Switchscreen_Click;
+            this.GoToMainPage.Click += GoToMainPage_Click;
         }
 
-        private void Switchscreen_Click(object sender, RoutedEventArgs e)
+        private void GoToMainPage_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(RegisterPage));
+            this.Frame.Navigate(typeof(MainPage));
         }
     }
 }
